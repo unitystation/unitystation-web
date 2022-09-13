993ac07e74f2d8097d5cf395cfc8a0d0b26015c9
Andrés Franco Riquelme Sandoval <andresfranco.rs@gmail.com>
2022-09-13 20:31:38 -0300

    feat: download link will now get the user's OS automatically

--- src/components/landing/landingButtons.tsx
+++ src/components/landing/landingButtons.tsx
@@ -1,10 +1,20 @@
 import Button from '../button';
-
-const downloadLink = 'https://github.com/unitystation/stationhub/releases';
+import getDownloadLink from '../../lib/platform';
 const githubUrl = 'https://github.com/unitystation/unitystation';
 const discordInviteUrl = 'https://discord.com/invite/tFcTpBp';
+const githubReleases = 'https://github.com/unitystation/stationhub/releases/latest';
+
+import {useEffect, useState} from 'react';
+
+
 
 const LandingButtons = () => {
+    const [downloadLink, setDownloadLink] = useState(githubReleases);
+
+    useEffect(() => {
+        getDownloadLink().then((link) => setDownloadLink(link));
+    }, []);
+
     return (<div className={'flex flex-wrap justify-center mt-8 gap-4'}>
             <Button filled={true} text={'Download'} linkTo={downloadLink}/>
             <Button filled={false} text={'Github'} linkTo={githubUrl}/>
