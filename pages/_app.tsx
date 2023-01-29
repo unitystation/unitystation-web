d5f9e681a13b98752baca7bb22696ed20470c247
Andrés Franco Riquelme Sandoval <andresfranco.rs@gmail.com>
2023-01-29 15:06:38 -0300

    feat(blog): first iteration of blog page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import '../styles/ck-content.css'
 import type {AppProps} from 'next/app'
 import Background from '../components/background';
 import Container from '../components/layout/container';
