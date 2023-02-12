07c42f18f87c9f54fa08c815e1d301a46d4f5bd2
Andrés Franco Riquelme Sandoval <andresfranco.rs@gmail.com>
2023-02-12 02:47:24 -0300

    feat(blog): made each post title a link to the blog post itself.

--- components/blog/post.tsx
+++ components/blog/post.tsx
@@ -1,9 +1,25 @@
 import {BlogPost} from "../../types/blogPost";
 import Section from "./section";
 import {toAgoTime} from "../../utils/timeUtils";
+import Link from "next/link";
+import Capsule from "../uiLibrary/capsule";
+import Dict = NodeJS.Dict;
+
+const colourByType: Dict<string> = {
+    'announcement': 'blue',
+    'weekly': 'indigo',
+    'community': 'green',
+}
+
+const textByType: Dict<string> = {
+    'announcement': 'Announcement',
+    'weekly': 'Weekly Update',
+    'community': 'Community Highlight',
+}
+
 
 const Post = (props: BlogPost) => {
-    const {title, author, date_created, sections} = props;
+    const {title, author, date_created, slug, type, sections} = props;
 
     return (
         <>
@@ -13,9 +29,11 @@ const Post = (props: BlogPost) => {
                     <address className={'flex items-center mb-6 not-italic'}>
                         <div className="inline-flex items-center mr-3 text-sm text-white">
                             <div>
-                                <h1 className="mb-4 text-4xl text-center font-extrabold leading-tight lg:mb-6 text-white">
-                                    {title}
-                                </h1>
+                                <Link href={`/blog/${slug}`}>
+                                    <h1 className="mb-4 text-4xl text-center font-extrabold leading-tight lg:mb-6 text-white hover:underline hover:cursor-pointer">
+                                        {title}
+                                    </h1>
+                                </Link>
                                 <p className="text-xl font-bold text-white">by {author}</p>
                                 <p className="text-base font-light text-gray-500 dark:text-gray-400">
                                     <time dateTime={date_created?.toString()} title={date_created?.toString()}>
