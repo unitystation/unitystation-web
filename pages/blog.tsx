14f27510f006cd98b6e540c598dc9f4b9badf517
Andrés Franco Riquelme Sandoval <andresfranco.rs@gmail.com>
2023-04-11 15:00:52 -0400

    feat: adds infinite scrolling for blogposts

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,22 +1,50 @@
 import {NextPage} from "next";
-import {useEffect, useState} from "react";
+import {useCallback, useEffect, useRef, useState} from "react";
 import Post from "../components/blog/post";
 import {BlogPostsResponse} from "../types/blogPostsResponse";
 import Head from "next/head";
 import Container from "../components/uiLibrary/container";
+import {BlogPost} from "../types/blogPost";
+import Spinner from "../components/uiLibrary/spinner";
 
 
 const Blog: NextPage = () => {
-    const [posts, setPosts] = useState({} as BlogPostsResponse);
-    const [currentPage, setCurrentPage] = useState(1);
+    const [postsResponse, setPostsResponse] = useState<BlogPostsResponse>();
+    const [posts, setPosts] = useState<BlogPost[]>([]);
     const [loading, setLoading] = useState(true);
+    const INITIAL_PAGE = "https://changelog.unitystation.org/posts/?page=1";
+
+    const fetchPosts = async (url: string) : Promise<BlogPostsResponse> => {
+        setLoading(true);
+        const response = await fetch(url);
+        return await response.json();
+    };
 
     useEffect(() => {
-        fetch(`https://changelog.unitystation.org/posts`)
-            .then(response => response.json())
-            .then(json => setPosts(json))
-            .then(() => setLoading(false))
-    }, [currentPage]);
+        fetchPosts(INITIAL_PAGE)
+            .then(json => {
+                setPostsResponse(json);
+                setPosts(json.results);
+            })
+            .finally(() => setLoading(false));
+    }, []);
+
+    const handleScroll = useCallback(async () => {
+        if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight || !postsResponse?.next) return;
+        setLoading(true);
+
+        const json = await fetchPosts(postsResponse.next);
+
+        setPosts((prevPosts) => [...prevPosts, ...json.results]);
+        setPostsResponse(json);
+
+        setLoading(false);
+    }, [postsResponse]);
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, [handleScroll]);
 
     return (
         <>
@@ -39,8 +67,9 @@ const Blog: NextPage = () => {
                 />
             </Head>
             <Container>
-                    {!loading && <div>
-                        {posts.results.map((post) => {
+                <ul id="posts">
+                    {posts &&
+                        posts.map((post) => {
                             return (
                                 <Post
                                     key={post.slug}
@@ -52,9 +81,10 @@ const Blog: NextPage = () => {
                                     state={post.state}
                                     type={post.type}
                                 />
-                            )
+                            );
                         })}
-                    </div>}
+                </ul>
+                {loading && <Spinner/>}
             </Container>
         </>
     )
