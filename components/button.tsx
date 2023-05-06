48c22f05bca7586b0d105207f0d907d0310588ca
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2023-05-07 02:25:59 +0300

    Adds Icons for buttons

--- components/button.tsx
+++ components/button.tsx
@@ -1,10 +1,14 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconDefinition } from '@fortawesome/free-solid-svg-icons';
+import { icon } from "@fortawesome/fontawesome-svg-core";
+
 type ButtonProps = {
-    filled: boolean, text: string, linkTo: string
+    filled: boolean, text: string, linkTo: string, buttonIcon?: IconDefinition,
 }
 
 
 const Button = (props: ButtonProps) => {
-    const {filled, text, linkTo} = props;
+    const {filled, text, linkTo, buttonIcon} = props;
     const filledClass = 'bg-gray-800 border-2 border-transparent text-white text-md mr-4 hover:bg-gray-900';
     const outlineClass = 'bg-transparent border-2 border-gray-800 text-white text-md hover:bg-gray-800';
 
@@ -12,6 +16,7 @@ const Button = (props: ButtonProps) => {
 
             <a href={linkTo}
                   className={`uppercase py-2 px-4 ${filled ? filledClass : outlineClass}` }>
+                {buttonIcon == null ? '' : <FontAwesomeIcon icon={buttonIcon}></FontAwesomeIcon>}
                 {text}
             </a>
     )
