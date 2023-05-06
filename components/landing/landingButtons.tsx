69963e86b9069ecfed672b1b8349cebbeda90ddd
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2023-05-07 02:50:06 +0300

    feat: separate dowload button from socials buttons and  add icon prop to buttons

--- components/landing/landingButtons.tsx
+++ components/landing/landingButtons.tsx
@@ -3,9 +3,10 @@ import getDownloadLink from '../../lib/platform';
 const githubUrl = 'https://github.com/unitystation/unitystation';
 const discordInviteUrl = 'https://discord.com/invite/tFcTpBp';
 const githubReleases = 'https://github.com/unitystation/stationhub/releases/latest';
+const patreonUrl = 'https://www.patreon.com/unitystation';
+import { faCloudDownload } from '@fortawesome/free-solid-svg-icons';
 
 import {useEffect, useState} from 'react';
-
 
 
 const LandingButtons = () => {
@@ -15,11 +16,17 @@ const LandingButtons = () => {
         getDownloadLink().then((link) => setDownloadLink(link));
     }, []);
 
-    return (<div className={'flex flex-wrap justify-center mt-8 gap-4'}>
-            <Button filled={true} text={'Download'} linkTo={downloadLink}/>
-            <Button filled={false} text={'Github'} linkTo={githubUrl}/>
-            <Button filled={false} text={'Discord'} linkTo={discordInviteUrl}/>
-        </div>)
+    return (
+        <>
+            <div className={'flex flex-wrap justify-center mt-8 gap-4'}>
+                <Button filled={true} text={'Download'} linkTo={downloadLink} buttonIcon={faCloudDownload}/>
+            </div>
+            <div className={'flex flex-wrap justify-center mt-8 gap-4'}>
+                <Button filled={false} text={'Github'} linkTo={githubUrl}/>
+                <Button filled={false} text={'Discord'} linkTo={discordInviteUrl}/>
+                <Button filled={false} text={'Patreon'} linkTo={patreonUrl}/>
+            </div>
+        </>)
 }
 
 export default LandingButtons;
