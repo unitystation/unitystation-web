0d7d26eee287f6847e56182fff28993ee54df478
Gilles <43683714+corp-0@users.noreply.github.com>
2024-02-24 22:54:16 -0300

    feat: allow panel to receive props such as className from outside

--- app/common/uiLibrary/panel.tsx
+++ app/common/uiLibrary/panel.tsx
@@ -6,11 +6,12 @@ interface PanelProps extends LayoutChildren {
 }
 
 export default function Panel(props: PanelProps) {
-    const { children, solid } = props;
+    const { children, solid, ...rest } = props;
     const className = classNames(
         "mx-auto w-full px-10 py-10 mb-8 format format-sm sm:format-base lg:format-lg format-invert bg-gray-900",{
             "bg-opacity-75": !solid,
-        }
+        },
+        rest.className
     );
 
     return (
