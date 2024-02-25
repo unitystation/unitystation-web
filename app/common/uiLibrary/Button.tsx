ecc72b699e02623bf57cd8d51361eb1235a57c45
Gilles <43683714+corp-0@users.noreply.github.com>
2024-02-24 22:55:18 -0300

    refactor: renamed button to LinkButton to better convey its use

--- app/common/uiLibrary/Button.tsx
+++ app/common/uiLibrary/Button.tsx
@@ -1,17 +1,17 @@
 import {IconType} from "react-icons";
 import classNames from "classnames";
+import LayoutChildren from "../../../types/layoutChildren";
 
-type ButtonProps = {
+interface ButtonProps extends LayoutChildren{
     filled: boolean,
-    text: string,
-    linkTo: string,
+    type: 'button' | 'submit' | 'reset',
     iconLeft?: IconType,
     iconRight?: IconType,
     className?: string
 }
 
 const Button = (props: ButtonProps) => {
-    const {filled, text, linkTo, iconLeft: IconLeft, iconRight: IconRight} = props;
+    const {filled, iconLeft: IconLeft, iconRight: IconRight, ...rest} = props;
     const filledClass = 'bg-gray-800 border-2 border-transparent text-white text-md mr-4 hover:bg-gray-900';
     const outlineClass = 'bg-gray-800 bg-opacity-30 border-2 border-gray-800 text-white text-md hover:bg-gray-800';
     const classes = classNames(
@@ -21,16 +21,9 @@ const Button = (props: ButtonProps) => {
     )
 
     return (
-            <a
-                href={linkTo}
-                className={classes}>
-                {IconLeft && <IconLeft className="mr-3 w-7 h-7" />}
-                <div>
-                    {text}
-                </div>
-                {IconRight && <IconRight className="ml-3 w-7 h-7" />}
-            </a>
+        <button className={classes}>{props.children}</button>
     )
-};
+
+}
 
 export default Button;
---
+++ app/common/uiLibrary/linkButton.tsx
@@ -0,0 +1,36 @@
+import {IconType} from "react-icons";
+import classNames from "classnames";
+
+type LinkButtonProps = {
+    filled: boolean,
+    text: string,
+    linkTo: string,
+    iconLeft?: IconType,
+    iconRight?: IconType,
+    className?: string
+}
+
+const LinkButton = (props: LinkButtonProps) => {
+    const {filled, text, linkTo, iconLeft: IconLeft, iconRight: IconRight} = props;
+    const filledClass = 'bg-gray-800 border-2 border-transparent text-white text-md mr-4 hover:bg-gray-900';
+    const outlineClass = 'bg-gray-800 bg-opacity-30 border-2 border-gray-800 text-white text-md hover:bg-gray-800';
+    const classes = classNames(
+        'uppercase py-2 px-4 flex justify-center items-center',
+        filled ? filledClass : outlineClass,
+        props.className
+    )
+
+    return (
+            <a
+                href={linkTo}
+                className={classes}>
+                {IconLeft && <IconLeft className="mr-3 w-7 h-7" />}
+                <div>
+                    {text}
+                </div>
+                {IconRight && <IconRight className="ml-3 w-7 h-7" />}
+            </a>
+    )
+};
+
+export default LinkButton;
