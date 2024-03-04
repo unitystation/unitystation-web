94050a497be217c043eccc9debba195e91e64dc3
Gilles <43683714+corp-0@users.noreply.github.com>
2024-03-04 15:51:51 -0300

    chore: remove default values from config file

--- next.config.js
+++ next.config.js
@@ -1,28 +1,26 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'unitystationfile.b-cdn.net',
-        port: '',
-        pathname: '/**/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn.discordapp.com',
-        port: '',
-        pathname: '/attachments/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'user-images.githubusercontent.com',
-        port: '',
-        pathname: '/**/**',
-      }
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'unitystationfile.b-cdn.net',
+                port: '',
+                pathname: '/**/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'cdn.discordapp.com',
+                port: '',
+                pathname: '/attachments/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'user-images.githubusercontent.com',
+                port: '',
+                pathname: '/**/**',
+            }
+        ],
+    },
 }
