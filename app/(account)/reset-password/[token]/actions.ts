d1d401be71ab5bbe82f81b31a431cfe1269c0d0b
Gilles <43683714+corp-0@users.noreply.github.com>
2024-03-09 15:54:03 -0300

    feat: add page to type your new password as a second step in password-reset process

---
+++ app/(account)/reset-password/[token]/actions.ts
@@ -0,0 +1,65 @@
+import {FieldError, GeneralError} from "../../../../lib/auth/guards";
+import {z} from "zod";
+
+export interface ResetPasswordStep2Response {
+    success: boolean;
+    error?: GeneralError | FieldError;
+}
+
+export const postPasswordReset = async (_: ResetPasswordStep2Response, formData: FormData): Promise<ResetPasswordStep2Response> => {
+    const schema = z.object({
+        password: z.string().min(6),
+        token: z.string().min(1)
+    });
+
+    const parsed = schema.safeParse({
+        password: formData.get('password'),
+        password2: formData.get('password2'),
+        token: formData.get('token')
+    });
+
+    if (!parsed.success) {
+        const fieldErrors = parsed.error.errors.reduce((acc, error) => {
+            return {...acc, [error.path[0]]: error.message}
+        }, {});
+
+        return {success: false, error: {status: 400, error: fieldErrors}};
+    }
+
+    const password2 = formData.get('password2');
+    if (password2 !== parsed.data.password) {
+        return {
+            success: false,
+            error:
+                {
+                    status: 400,
+                    error: {
+                        password2: ['Passwords do not match']
+                    }
+                }
+        }
+    }
+
+    const body = {
+        password: parsed.data.password,
+        token: parsed.data.token
+    }
+
+    try {
+        const response = await fetch(`${process.env.CC_API_URL}/accounts/reset-password/${parsed.data.token}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body),
+        });
+
+        if (!response.ok) {
+            return { success: false, error: {status: 400, error: {password: ['An unexpected error occurred']}}};
+        }
+
+        return { success: true};
+    } catch (error) {
+        return { success: false, error: {status: 400, error: {password: ['An unexpected error occurred']}}};
+    }
+}
