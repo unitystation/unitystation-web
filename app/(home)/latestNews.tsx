6c9f52b4212ae175cc1a90191a77a2e5c06cb928
Gilles <43683714+corp-0@users.noreply.github.com>
2024-10-12 02:35:29 -0300

    feat: slight redesign of landing page

--- app/(home)/latestNews.tsx
+++ app/(home)/latestNews.tsx
@@ -1,10 +1,10 @@
 import {BlogPost} from "../../types/blogPost";
 import classNames from "classnames";
 import Container from "../common/uiLibrary/container";
-import {toAgoTime} from "../../utils/timeUtils";
 import PageSectionTitle from "../common/uiLibrary/pageSectionTitle";
+import Button from "../common/uiLibrary/Button";
 
-interface NewsCardProps {
+interface PostPreviewCardProps {
     post: BlogPost,
     isMain?: boolean,
     className?: string
@@ -12,67 +12,111 @@ interface NewsCardProps {
 
 interface LatestNewsProps {
     posts: BlogPost[]
+    className?: string
 }
 
-const NewsCard = (props: NewsCardProps) => {
-    const {title, summary, socials_image, date_created, slug} = props.post;
-    const {className: classes} = props;
-    const isMain = props.isMain ?? false;
+const PostPreviewImage = ({post, isMain = false, className}: PostPreviewCardProps) => {
+    const outerContainerStyles = classNames(
+        {'flex-1 overflow-hidden': isMain},
+        {'w-full xl:w-1/3 overflow-hidden rounded-lg': !isMain},
+    )
 
-    const style = {
-        backgroundImage: `url(${socials_image})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        backgroundBlendMode: 'multiply',
-        backgroundPosition: 'center',
-    }
-
-    const truncateSummary = (summary: string, isMain: boolean) => {
-        if (isMain) {
-            return summary;
-        }
-
-        return summary.slice(0, 175) + '...';
-    }
+    const innerContainerStyles = classNames(
+        {'w-full h-32 lg:h-40': !isMain},
+        'overflow-hidden', 'rounded-lg',
+        {'h-full' : isMain}
+    )
 
     return (
-            <a href={`/blog/${slug}`} className={classNames('relative overflow-hidden', classes)}>
-                <div className="absolute inset-0 rounded-lg shadow-lg bg-gray-400" style={style}></div>
-                <div className="relative p-3 h-full flex flex-col justify-between">
-                    <div>
-                        <div
-                            className={
-                            classNames('text-2xl text-white leading-tight border-b hover:border-dashed hover:border-gray-500',
-                                {'lg:text-5xl': isMain})}>{title}</div>
-                        <div className="text-normal text-gray-300">
-                            <span className=" pb-1">{toAgoTime(date_created)}</span>
-                        </div>
-                    </div>
-                    <p className="">{truncateSummary(summary, isMain)}</p>
-                </div>
-            </a>
+        <div className={classNames(outerContainerStyles, {className})}>
+            <div className={innerContainerStyles}>
+                <img
+                    src={post.socials_image}
+                    alt={post.title}
+                    className="w-full h-full object-cover transition-transform duration-300 ease-in-out transform group-hover:scale-105"
+                    loading="lazy"
+                />
+            </div>
+        </div>
     )
 }
 
-const LatestNews = (props: LatestNewsProps) => {
-    const {posts} = props;
+const PostPreviewBody = ({post, isMain = false, className}: PostPreviewCardProps) => {
+    const truncateSummary = (text: string) => {
+        const maxChars = 100;
+
+        if (isMain) {
+            return text;
+        }
+        return text.length > maxChars ? text.slice(0, maxChars - 3) + '...' : text;
+    };
+
+    const styles = classNames(
+        'mt-4',
+        {'lg:mt-0 lg:ml-4 lg:flex-1': !isMain}
+
+    );
+
+    const titleStyles = classNames(
+        {'text-xl': isMain},
+        {'text-l': !isMain},
+        'font-bold text-white-800 mb-2'
+    );
 
     return (
-        <>
+        <div className={classNames(styles, {className})}>
+            <h3 className={titleStyles}>{post.title}</h3>
+            <p className="text-sm text-gray-600 mb-2">{new Date(post.date_created).toLocaleDateString()}</p>
+            <p className="text-gray-400">{truncateSummary(post.summary)}</p>
+        </div>
+    )
+}
+
+const PostPreviewCard = ({post, isMain}: PostPreviewCardProps) => {
+    const style = classNames(
+        'flex',
+        'flex-col',
+        'flex-1',
+        'hover:border-l hover:border-r hover:border-b hover:border-t hover:border-gray-700 hover:rounded-lg',
+        'group',
+        'mb-8 xl:mb-0',
+        {'xl:max-w-[600px]': isMain},
+        {'xl:max-w-[550px] xl:flex-row': !isMain},
+    );
+
+    return (
+        <a href={`/blog/${post.slug}`} className={style}>
+            <PostPreviewImage post={post}  isMain={isMain} />
+            <PostPreviewBody post={post} isMain={isMain} />
+        </a>
+    );
+};
+
+const LatestNews = (props: LatestNewsProps) => {
+    const {posts, className} = props;
+
+    return (
+        <div className={className}>
             <PageSectionTitle>Latest News</PageSectionTitle>
             <Container>
-                <div className="flex flex-col sm:flex-col md:flex-col lg:flex-row gap-5">
-                    <div className="w-full lg:w-1/2 flex flex-col">
-                        <NewsCard post={posts[0]} isMain className="h-full" />
+                <div className="flex flex-col gap-6">
+                    <div className="flex flex-col xl:flex-row gap-6 justify-center">
+                        <PostPreviewCard post={posts[0]} isMain={true} className="flex flex-col"/>
+
+                        <div className="flex flex-col justify-between gap-6 xl:gap-0">
+                            {posts.slice(1).map((post) => (
+                                <PostPreviewCard key={post.slug} post={post} isMain={false}/>
+                            ))}
+                        </div>
                     </div>
-                    <div className="w-full lg:w-1/2 flex flex-col gap-4">
-                        {posts.slice(1).map((post, index) => (
-                            <NewsCard key={index} post={post} className="h-auto flex-grow" />
-                        ))}
+                    <div className="flex justify-end px-20">
+                        <Button filled={false} >
+                            Read More
+                        </Button>
                     </div>
                 </div>
             </Container>
-        </>
+        </div>
     );
 }
 
