a2f44564ef17f463dbe488d6b707a299b70fe29b
Gilles <43683714+corp-0@users.noreply.github.com>
2024-10-27 18:12:12 -0300

    feat: Button component now supports choosing uppercase and side icons.

--- app/common/uiLibrary/Button.tsx
+++ app/common/uiLibrary/Button.tsx
@@ -3,7 +3,8 @@ import classNames from "classnames";
 import React from "react";
 
 interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement>{
-    filled: boolean,
+    filled?: boolean,
+    upperCase?: boolean,
     type?: 'button' | 'submit' | 'reset',
     iconLeft?: IconType,
     iconRight?: IconType,
@@ -11,19 +12,23 @@ interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement>{
 }
 
 const Button = (props: ButtonProps) => {
-    const {type = 'button', filled, iconLeft: IconLeft, iconRight: IconRight} = props;
+    const {type = 'button', filled = true, upperCase = true, iconLeft: IconLeft, iconRight: IconRight} = props;
     const filledClass = 'bg-gray-800 border-2 border-transparent text-white text-md mr-4 hover:bg-gray-900';
     const outlineClass = 'bg-gray-800 bg-opacity-30 border-2 border-gray-800 text-white text-md hover:bg-gray-800';
     const classes = classNames(
-        'uppercase py-2 px-4 flex justify-center items-center',
+        'py-2 px-4 flex justify-center items-center',
+        {'uppercase': upperCase},
         filled ? filledClass : outlineClass,
         props.className
     )
 
     return (
-        <button onClick={props.onClick} type={type} className={classes}>{props.children}</button>
+        <button onClick={props.onClick} type={type} className={classes}>
+            {IconLeft && <IconLeft className="mr-3 w-7 h-7" />}
+            {props.children}
+            {IconRight && <IconRight className="ml-3 w-7 h-7" />}
+        </button>
     )
-
 }
 
 export default Button;
