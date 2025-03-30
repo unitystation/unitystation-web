58c2f711bf4e73a210bd65e47da4ff03561b3675
Gilles <43683714+corp-0@users.noreply.github.com>
2025-03-29 23:20:36 -0300

    chore: update packages

--- utils/platform.ts
+++ utils/platform.ts
@@ -1,4 +1,4 @@
-import UAParser from 'ua-parser-js';
+import {UAParser} from 'ua-parser-js';
 import GithubReleaseResponse from '../types/githubReleaseResponse';
 
 function getUserPlatform(): string | undefined {
